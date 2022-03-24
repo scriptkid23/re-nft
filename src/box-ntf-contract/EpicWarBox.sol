94afd7c5d2df1fd33db237be59b445c49d875bc5
Hoan Do <sieunhannhenkutesn@gmail.com>
2022-03-24 15:15:02 +0700

    refactor: update open selected

--- src/box-ntf-contract/EpicWarBox.sol
+++ src/box-ntf-contract/EpicWarBox.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
@@ -24,8 +23,12 @@ interface IEpicWarNFT {
     function mintNft(address receiver, uint256 nftId) external;
 }
 
-
-contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgradeable, ERC721HolderUpgradeable {
+contract EpicWarBox is
+    Initializable,
+    OwnableUpgradeable,
+    ERC721EnumerableUpgradeable,
+    ERC721HolderUpgradeable
+{
     struct EventInfo {
         uint256 totalSupply;
         uint256 boxPrice;
@@ -56,18 +59,42 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
     mapping(uint256 => mapping(address => uint256)) public userBought; //number of box buy in event
     mapping(uint256 => uint256) public randomNumber; //random number by event
 
-
     string public uri;
     address public signer;
     address public fundWallet;
     address public VRFContract;
     bool public allowTransfer;
     uint256 public eventStartId;
-    
-    event EventCreated(uint256 totalSupply, uint256 price, address currency, uint256 startTime, uint256 endTime, uint256 maxBuy, uint256 startID, uint256 openBoxTime, address nftContract);
-    event BoxCreated(uint256 indexed id, address boxOwner, uint256 eventId, string eventType, string boxUri, string boxName, address boxContractAddress, uint256 price, address currency);
-    event BoxOpened(address indexed user, uint256 boxId, uint256 nftId, uint256 eventId);
-    
+
+    event EventCreated(
+        uint256 totalSupply,
+        uint256 price,
+        address currency,
+        uint256 startTime,
+        uint256 endTime,
+        uint256 maxBuy,
+        uint256 startID,
+        uint256 openBoxTime,
+        address nftContract
+    );
+    event BoxCreated(
+        uint256 indexed id,
+        address boxOwner,
+        uint256 eventId,
+        string eventType,
+        string boxUri,
+        string boxName,
+        address boxContractAddress,
+        uint256 price,
+        address currency
+    );
+    event BoxOpened(
+        address indexed user,
+        uint256 boxId,
+        uint256 nftId,
+        uint256 eventId
+    );
+
     function __EpicWarBox_init(
         string memory name,
         string memory symbol,
@@ -103,65 +130,106 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
         require(_startTime < _endTime, "Invalid time");
         require(_maxBuy > 0, "Need set max buy");
 
-        eventByID[_eventID] = EventInfo(_totalSupply, _price, _currency, _startTime, _endTime, _maxBuy, eventStartId, _openBoxTime, _nftContract, _eventType, 0);
+        eventByID[_eventID] = EventInfo(
+            _totalSupply,
+            _price,
+            _currency,
+            _startTime,
+            _endTime,
+            _maxBuy,
+            eventStartId,
+            _openBoxTime,
+            _nftContract,
+            _eventType,
+            0
+        );
         eventStartId += _totalSupply;
         emit EventCreated(
             _totalSupply,
-            _price, _currency,
-            _startTime, _endTime,
+            _price,
+            _currency,
+            _startTime,
+            _endTime,
             _maxBuy,
             eventStartId,
             _openBoxTime,
             _nftContract
         );
     }
-    
+
     function addBox(
         uint256 _eventID,
         uint256[] memory _quantity,
         string[] memory _boxName,
         string[] memory _boxImageUri
     ) public onlyOwner {
-        require(_quantity.length == _boxName.length && _boxName.length == _boxImageUri.length, "Box data error");
+        require(
+            _quantity.length == _boxName.length &&
+                _boxName.length == _boxImageUri.length,
+            "Box data error"
+        );
         for (uint256 index = 0; index < _quantity.length; index++) {
-
-            boxesByEvent[_eventID][index] = BoxList(_quantity[index], 0, _boxName[index], _boxImageUri[index]);
+            boxesByEvent[_eventID][index] = BoxList(
+                _quantity[index],
+                0,
+                _boxName[index],
+                _boxImageUri[index]
+            );
         }
     }
 
-
-    function buyBox(uint256 _eventID, uint256 _amount, uint256 _indexBoxList, address _token) public payable {
+    function buyBox(
+        uint256 _eventID,
+        uint256 _amount,
+        uint256 _indexBoxList,
+        address _token
+    ) public payable {
         EventInfo storage eventInfo = eventByID[_eventID];
 
-        require(_amount > 0 && _amount + userBought[_eventID][msg.sender] <= eventInfo.maxBuy, "Rate limit exceeded");
+        require(
+            _amount > 0 &&
+                _amount + userBought[_eventID][msg.sender] <= eventInfo.maxBuy,
+            "Rate limit exceeded"
+        );
         require(block.timestamp >= eventInfo.startTime, "Sale has not started");
         require(block.timestamp <= eventInfo.endTime, "Sale has ended");
         require(_token == eventInfo.currency, "Invalid token");
         BoxList storage boxes = boxesByEvent[_eventID][_indexBoxList];
 
         require(boxes.quantity - boxes.bought >= _amount, "sold out");
-        
+
         uint256 totalFund = eventInfo.boxPrice * _amount;
         if (_token == address(0)) {
             require(totalFund == msg.value, "invalid value");
         }
-        
+
         _fowardFund(totalFund, _token);
-        for (uint i = 0; i < _amount; i++) {
+        for (uint256 i = 0; i < _amount; i++) {
             uint256 boxID = eventInfo.boxCount + eventInfo.startID + 1;
             _safeMint(msg.sender, boxID);
             boxByEvent[boxID] = _eventID;
             userBought[_eventID][msg.sender] += 1;
             eventInfo.boxCount += 1;
-            emit BoxCreated(boxID, msg.sender, _eventID, eventInfo.eventType, boxes.uriImage, boxes.nameBox, address(this), eventInfo.boxPrice, eventInfo.currency);
+            emit BoxCreated(
+                boxID,
+                msg.sender,
+                _eventID,
+                eventInfo.eventType,
+                boxes.uriImage,
+                boxes.nameBox,
+                address(this),
+                eventInfo.boxPrice,
+                eventInfo.currency
+            );
         }
 
         boxes.bought += _amount;
     }
 
     function _fowardFund(uint256 _amount, address _token) internal {
-        if (_token == address(0)) { // native token (BNB)
-            (bool isSuccess,) = fundWallet.call{value: _amount}("");
+        if (_token == address(0)) {
+            // native token (BNB)
+            (bool isSuccess, ) = fundWallet.call{value: _amount}("");
             require(isSuccess, "Transfer failed: gas error");
             return;
         }
@@ -172,7 +240,7 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
     function setAllowTransfer(bool allow) public onlyOwner {
         allowTransfer = allow;
     }
-    
+
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -185,33 +253,53 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function updateOpenBoxInfo(uint256 _eventID, uint256 _openBoxTime, address _nftContract) public onlyOwner {
+    function updateOpenBoxInfo(
+        uint256 _eventID,
+        uint256 _openBoxTime,
+        address _nftContract
+    ) public onlyOwner {
         EventInfo storage eventInfo = eventByID[_eventID];
         eventInfo.openBoxTime = _openBoxTime;
         eventInfo.nftContract = _nftContract;
     }
 
-    function updateEventTimeInfo(uint256 _eventID, uint256 _startTime, uint256 _endTime) public onlyOwner {
+    function updateEventTimeInfo(
+        uint256 _eventID,
+        uint256 _startTime,
+        uint256 _endTime
+    ) public onlyOwner {
         require(_startTime < _endTime, "Invalid time");
         EventInfo storage eventInfo = eventByID[_eventID];
         eventInfo.startTime = _startTime;
         eventInfo.endTime = _endTime;
     }
 
-    function updateEventPriceInfo(uint256 _eventID, uint256 _price, address _currency) public onlyOwner {
+    function updateEventPriceInfo(
+        uint256 _eventID,
+        uint256 _price,
+        address _currency
+    ) public onlyOwner {
         EventInfo storage eventInfo = eventByID[_eventID];
         eventInfo.boxPrice = _price;
         eventInfo.currency = _currency;
     }
 
-    function updateEventTotalSupplyInfo(uint256 _eventID, uint256 _totalSupply, uint256 _startID, uint256 _maxBuy) public onlyOwner {
+    function updateEventTotalSupplyInfo(
+        uint256 _eventID,
+        uint256 _totalSupply,
+        uint256 _startID,
+        uint256 _maxBuy
+    ) public onlyOwner {
         EventInfo storage eventInfo = eventByID[_eventID];
         eventInfo.totalSupply = _totalSupply;
         eventInfo.startID = _startID;
         eventInfo.maxBuy = _maxBuy;
     }
 
-    function updateBoxEventCount(uint256 _eventID, uint256 _count) public onlyOwner {
+    function updateBoxEventCount(uint256 _eventID, uint256 _count)
+        public
+        onlyOwner
+    {
         EventInfo storage eventInfo = eventByID[_eventID];
         eventInfo.boxCount = _count;
     }
@@ -239,7 +327,7 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
     }
 
     //call when create new event after mint nft from game to update eventStartId
-    function setEventStartId(uint256 _startId) public onlyOwner { 
+    function setEventStartId(uint256 _startId) public onlyOwner {
         eventStartId = _startId;
     }
 
@@ -247,10 +335,14 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
     function openBox(uint256 _boxId, uint256 _eventID) public {
         require(boxByEvent[_boxId] == _eventID, "Box not in event");
         EventInfo storage eventInfo = eventByID[_eventID];
-        require(ownerOf(_boxId) == msg.sender, "User must be owner of boxId");
-        require(block.timestamp >= eventInfo.openBoxTime, "Open box has not started");
+        require(ownerOf(_boxId) == msg.sender, "User must be owner of box!");
+        require(
+            block.timestamp >= eventInfo.openBoxTime,
+            "Open box has not started"
+        );
         uint256 rand = randomNumber[_eventID] % eventInfo.totalSupply;
-        uint256 nftId = (_boxId + rand) % eventInfo.totalSupply + eventInfo.startID; //random boxId to nftId
+        uint256 nftId = ((_boxId + rand) % eventInfo.totalSupply) +
+            eventInfo.startID; //random boxId to nftId
 
         //mint nft from nft contract & burn box nft
         IEpicWarNFT(eventInfo.nftContract).mintNft(msg.sender, nftId);
@@ -258,24 +350,37 @@ contract EpicWarBox is Initializable, OwnableUpgradeable, ERC721EnumerableUpgrad
         emit BoxOpened(msg.sender, _boxId, nftId, _eventID);
     }
 
-    function emergencyWithdrawNFT(uint256 _eventID, uint256 _id, address _to) public onlyOwner {
+    function emergencyWithdrawNFT(
+        uint256 _eventID,
+        uint256 _id,
+        address _to
+    ) public onlyOwner {
         // transfer NFT from owner
         EventInfo storage eventInfo = eventByID[_eventID];
-        IERC721(eventInfo.nftContract).safeTransferFrom(address(this), _to, _id);
+        IERC721(eventInfo.nftContract).safeTransferFrom(
+            address(this),
+            _to,
+            _id
+        );
     }
 
-    function openAllBox(uint256 _eventID) public {
+    function openSelectedBox(uint256[] memory nftIds) public {
+        require(nftIds.length > 0, "Not open any box?");
+        for (uint256 index = 0; index < nftIds.length; index++) {
+            require(
+                ownerOf(nftIds[index]) == msg.sender,
+                "User must be owner of box!"
+            );
+            openBox(nftIds[index], boxByEvent[nftIds[index]]);
+        }
+    }
+
+    function openAllBox() public {
         uint256 userBox = balanceOf(msg.sender);
         require(userBox > 0, "User not owner of any box");
         for (uint256 index = 0; index < userBox; index++) {
-            uint256 currentBalance = balanceOf(msg.sender);
-            if (currentBalance == 0) {
-                continue;
-            }
-            uint256 boxId = tokenOfOwnerByIndex(msg.sender, currentBalance - 1);
-            if (boxByEvent[boxId] == _eventID) {
-                openBox(boxId, _eventID);
-            }
+            uint256 boxId = tokenOfOwnerByIndex(msg.sender, 0);
+            openBox(boxId, boxByEvent[boxId]);
         }
     }
 }
